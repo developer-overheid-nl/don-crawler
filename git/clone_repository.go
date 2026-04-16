ddf1c05badad5cc7b638adf72dcd1ee49fa8512f
pasibun <matthijs_hovestad@hotmail.com>
2026-04-16 13:09:19 +0200

    feat: add placeholder for GitLab authentication in withAuthToken function

--- git/clone_repository.go
+++ git/clone_repository.go
@@ -92,6 +92,8 @@ func withAuthToken(hostname, _ string) (transport.AuthMethod, error) {
 
 		return nil, errors.New("github app auth not configured for github.com")
 	case "gitlab.com":
+		
+		//nolint
 		return nil, nil
 	default:
 		// No-op for other hosts.
